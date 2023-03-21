fce9e5732951850c85ba1fd38c45d7103957a752
Pouriya Khazaei <pouriya.khazaei@arvancloud.com>
2023-03-22 01:06:12 +0330

    add: create transaction event

---
+++ app/Events/CreateTransactionEvent.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Transaction;
+use App\Models\User;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class CreateTransactionEvent
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public Transaction $transaction;
+    public User $user;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(Transaction $transaction, User $user)
+    {
+        $this->user = $user;
+        $this->transaction = $transaction;
+    }
+}
