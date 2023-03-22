c7173351f7935825614dec56311cb4d91c987ee8
Pouriya Khazaei <pouriya.khazaei@arvancloud.com>
2023-03-22 23:49:37 +0330

    add: sweet alert styles and scripts to app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,8 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="{{ asset('node_modules/sweetalert2/dist/sweetalert2.min.css') }}">
+        <script src="{{ asset('node_modules/sweetalert2/dist/sweetalert2.min.js') }}"></script>
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
