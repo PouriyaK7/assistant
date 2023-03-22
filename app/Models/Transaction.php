518962e98d1e606e30c3dd32ee0eefa3039cbcb4
Pouriya Khazaei <pouriya.khazaei@arvancloud.com>
2023-03-23 01:13:32 +0330

    add: formatting amount attribute in transaction model

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -10,8 +10,23 @@ class Transaction extends Model
     protected $guarded = [];
     public $incrementing = false;
 
+    /**
+     * Get user related to transaction
+     *
+     * @return BelongsTo
+     */
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Format amount attribute
+     *
+     * @return string
+     */
+    public function getAmountAttribute(): string
+    {
+        return number_format($this->attributes['amount']);
     }
 }
