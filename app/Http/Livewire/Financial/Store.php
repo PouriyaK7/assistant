cb7c5c313205412fc936de5544532ecde5502ed9
Pouriya Khazaei <pouriya.khazaei@arvancloud.com>
2023-03-23 01:15:23 +0330

    add: store method to financial store component

--- app/Http/Livewire/Financial/Store.php
+++ app/Http/Livewire/Financial/Store.php
@@ -2,16 +2,60 @@
 
 namespace App\Http\Livewire\Financial;
 
+use App\Services\Livewire\ConfirmSubmit;
+use App\Services\Livewire\HasAlert;
+use App\Services\Livewire\HasModal;
+use App\Services\TransactionService;
 use Illuminate\Contracts\View\View;
 use Livewire\Component;
 
 class Store extends Component
 {
-    public int $amount;
+    use HasModal, ConfirmSubmit, HasAlert;
 
-    public function store()
+    # Form states
+    public string $title;
+    public string $amount;
+
+    # Transaction service instance
+    protected TransactionService $service;
+
+    # Listeners for ConfirmSubmit trait
+    protected $listeners = ['store' => 'store'];
+
+    # Storing transaction validation rules
+    protected array $rules = [
+        'title' => ['string', 'required', 'min:0'],
+        'amount' => ['required', 'integer'],
+    ];
+
+    public function __construct($id = null)
     {
-        //
+        # Initialize transaction service
+        $this->service = app()->make('transaction.service');
+
+        parent::__construct($id);
+    }
+
+    /**
+     * Store new transaction in db
+     *
+     * @return void
+     */
+    public function store(): void
+    {
+        # Create transaction with given data
+        $this->service->create($this->title, $this->amount, auth()->id());
+
+        # Show error alert on failure
+        if (empty($this->service->get())) {
+            $this->showAlert('Failed to create transaction', $this->icons['error']);
+            return;
+        }
+
+        # Reload page on success
+        $this->showAlert('Created transaction successfully', $this->icons['success']);
+        $this->redirect(route('financial'));
     }
 
     public function render(): View
