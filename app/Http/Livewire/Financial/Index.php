3275b6f02a37c02fc98a35194dd635f08d340737
Pouriya Khazaei <pouriya.khazaei@arvancloud.com>
2023-03-24 23:06:13 +0330

    add: delete to index component of transactions

--- app/Http/Livewire/Financial/Index.php
+++ app/Http/Livewire/Financial/Index.php
@@ -2,12 +2,36 @@
 
 namespace App\Http\Livewire\Financial;
 
+use App\Events\UpdateTransactionEvent;
+use App\Models\Transaction;
+use App\Services\Livewire\HasAlert;
+use App\Services\TransactionService;
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Index extends Component
 {
-    public function render()
+    use HasAlert;
+
+    /**
+     * Delete transaction from db and increase the amount from user balance
+     *
+     * @param Transaction $transaction
+     * @return void
+     */
+    public function delete(Transaction $transaction): void
     {
-        return view('livewire.financial.index');
+        $service = new TransactionService($transaction);
+        $amount = $service->delete();
+        event(new UpdateTransactionEvent($amount, auth()->id()));
+
+        $this->showAlert('Transaction deleted successfully', $this->icons['success']);
+    }
+
+    public function render(): View
+    {
+        $transactions = Auth::user()->transactions()->paginate(20);
+        return view('livewire.financial.index', compact('transactions'));
     }
 }
