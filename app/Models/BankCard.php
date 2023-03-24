2638d57f2679623550fe0831a964a32e4273da45
Pouriya Khazaei <pouriya.khazaei@arvancloud.com>
2023-03-25 02:12:33 +0330

    add: bank card db model

---
+++ app/Models/BankCard.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class BankCard extends Model
+{
+    protected $guarded = [];
+
+    /**
+     * Get all transactions of the bank card
+     *
+     * @return HasMany
+     */
+    public function transactions(): HasMany
+    {
+        return $this->hasMany(Transaction::class);
+    }
+
+    /**
+     * Get user related to bank card
+     *
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
