f09033dca932792c50a524c10b80152ee2fdde84
Pouriya Khazaei <pouriya.khazaei@arvancloud.com>
2023-03-25 14:18:56 +0330

    add: bank card update modal

--- resources/views/livewire/bank-card/update.blade.php
+++ resources/views/livewire/bank-card/update.blade.php
@@ -1,3 +1,40 @@
 <div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
+    <x-button wire:click="showModal()" id="update-modal">Update</x-button>
+    <x-dialog-modal wire:model="modalStatus">
+        <x-slot name="title">Edit Bank Card</x-slot>
+        <x-slot name="content">
+            <x-slot name="logo">
+                <x-authentication-card-logo/>
+            </x-slot>
+
+            <x-validation-errors class="mb-4"/>
+
+            @if (session('status'))
+                <div class="mb-4 font-medium text-sm text-green-600">
+                    {{ session('status') }}
+                </div>
+            @endif
+
+            <form wire:submit.prevent="update">
+                @csrf
+
+                <div class="mt-3">
+                    <x-label for="title" value="{{ __('Title') }}"/>
+                    <x-input id="title" class="block mt-1 w-full" type="text" name="title" :value="old('title')"
+                             required autofocus autocomplete="title" wire:model="title" />
+                </div>
+
+                <div class="mt-3">
+                    <x-label for="number" value="{{ __('Number') }}"/>
+                    <x-input id="number" class="block mt-1 w-full" type="text" name="number" :value="old('number')"
+                             required autofocus autocomplete="number" wire:model="number" />
+                </div>
+
+                <div class="mt-3">
+                    <x-button>Submit</x-button>
+                </div>
+            </form>
+        </x-slot>
+        <x-slot name="footer"></x-slot>
+    </x-dialog-modal>
 </div>
