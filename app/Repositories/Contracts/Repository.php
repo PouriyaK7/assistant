ef2815a5de4553fb118942701ab57cfa3945b1b7
Pouriya Khazaei <pouriya.khazaei@arvancloud.com>
2023-03-29 19:14:13 +0330

    add: repository interface

---
+++ app/Repositories/Contracts/Repository.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Repositories\Contracts;
+
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+interface Repository
+{
+    /**
+     * Get all resources from db table with sort and search options
+     *
+     * @param array $sort
+     * @param array $search
+     * @return Collection
+     */
+    public function all(array $sort = [], array $search = []): Collection;
+
+    /**
+     * Fetch specific record from db with given field and value
+     *
+     * @param string $id
+     * @param string $field
+     * @return null|Model
+     */
+    public function get(string $id, string $field = 'id'): ?Model;
+
+    /**
+     * Update specific record from db
+     *
+     * @param string $id
+     * @param array $data
+     * @param string $field
+     * @return bool
+     */
+    public function update(string $id, array $data, string $field = 'id'): bool;
+
+    /**
+     * Store new resource in db
+     *
+     * @param array $data
+     * @return null|Model
+     */
+    public function store(array $data): ?Model;
+
+    /**
+     * Delete record from db
+     *
+     * @param string $id
+     * @param string $field
+     * @return bool
+     */
+    public function delete(string $id, string $field = 'id'): bool;
+}
